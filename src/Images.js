2ed2b8c981aedd1456fa63f955a4f4503f0550e7
suzStar <114248187+suzStar@users.noreply.github.com>
2023-03-13 21:50:43 +0000

    Pexels API bug

--- src/Images.js
+++ src/Images.js
@@ -1,8 +1,25 @@
 import React from "react";
+//import "./Images.css";
 
 function Images(props) {
-  console.log(props.photo);
-  return <div>hello</div>;
+  console.log(props.photos);
+  return (
+    <div className="row">
+      {props.photos.map(function (photo, index) {
+        return (
+          <div className="col-2" key={index}>
+            <a href={photo.url} target="_blank" rel="noreferrer">
+              <img
+                className="img-fluid"
+                src={photo.src.tiny}
+                alt={photo.alt}
+              ></img>
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Images;
--- src/Meaning.js
+++ src/Meaning.js
@@ -7,7 +7,7 @@ import Images from "./Images";
 function Meanings(props) {
   return (
     <div className="Meanings row gx-1">
-      <div className=" col-9">
+      <div className=" col-8">
         <div className="row justify-content-start">
           <SpeechMeaning definitions={props.meaningData} />
           <Example example={props.meaningData.example} />
@@ -15,8 +15,8 @@ function Meanings(props) {
         </div>
         <hr />
       </div>
-      <div className="col-3">
-        <Images photo={props.photos} />
+      <div className="col-4">
+        <Images photos={props.photos} />
       </div>
     </div>
   );
