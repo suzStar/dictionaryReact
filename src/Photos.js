5b7f25cb45d3a68e59210fd538b8bd14bcd31347
suzStar <114248187+suzStar@users.noreply.github.com>
2023-03-14 16:57:44 +0000

    Bug fix

--- src/Photos.js
+++ src/Photos.js
@@ -2,23 +2,27 @@ import React from "react";
 import "./Photos.css";
 
 function Photos(props) {
-  return (
-    <div className="Photos imageContainer">
-      {props.photos.photos.map(function (photo, index) {
-        return (
-          <div key={index}>
-            <a href={photo.url} target="_blank" rel="noreferrer">
-              <img
-                className="img-fluid"
-                src={photo.src.landscape}
-                alt={photo.alt}
-              ></img>
-            </a>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (props.photos) {
+    return (
+      <div className="Photos imageContainer">
+        {props.photos.photos.map(function (photo, index) {
+          return (
+            <div key={index}>
+              <a href={photo.url} target="_blank" rel="noreferrer">
+                <img
+                  className="img-fluid"
+                  src={photo.src.landscape}
+                  alt={photo.alt}
+                ></img>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Photos;
